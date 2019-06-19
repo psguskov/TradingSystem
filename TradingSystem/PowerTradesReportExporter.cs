6cd5d60893b662ab55625b10be5e6874f6b3a63f
Pavel Guskov <Pavel_Guskov1@epam.com>
2019-06-19 12:58:29 +0300

    Handle case if exported file with the same name already exist (case of long day)

--- TradingSystem/PowerTradesReportExporter.cs
+++ TradingSystem/PowerTradesReportExporter.cs
@@ -26,6 +26,13 @@ namespace TradingSystem
                     Directory.CreateDirectory(directoryPath);
                 }
 
+                int suffix = 1;
+                while(File.Exists(fullPath))
+                {
+                    fullPath = CombineFullPathToExport(directoryPath, aggregatedTrade, suffix.ToString());
+                    suffix++;
+                }
+
                 using (var writer = new StreamWriter(fullPath))
                 using (var csv = new CsvWriter(writer))
                 {
@@ -38,14 +45,18 @@ namespace TradingSystem
             }
         }
 
-        private string CombineFullPathToExport(string directoryPath, PowerTrade aggregatedTrade)
+        private string CombineFullPathToExport(string directoryPath, PowerTrade aggregatedTrade, string suffix = "")
         {
             string dateDayPart = aggregatedTrade.CreatedDate.ToString("yyyyMMdd");
             string dateTimePart = aggregatedTrade.CreatedDate.TimeOfDay.ToString("hhmm");
             string extension = "csv";
+            if (!String.IsNullOrWhiteSpace(suffix))
+            {
+                suffix = $"_{suffix}";
+            }
             return $"{directoryPath}\\PowerPosition_" +
                     $"{dateDayPart}_" +
-                    $"{dateTimePart}.{extension}";
+                    $"{dateTimePart}{suffix}.{extension}";
         }
     }
 }
